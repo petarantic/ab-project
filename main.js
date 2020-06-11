bb26c94dda418e7afa6d73824e3f9cf2f40b6b6d
petarantic <57760707+petarantic@users.noreply.github.com>
2020-06-11 11:34:14 +0200

    nesto nesto

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-const caption = document.getElementsByClassName('caption');
+const product = document.getElementsByClassName('product');
 
-for (var i = 0; i < caption.lenght; i++) {
-		caption[i].addEventListener('click', function(e) {
-			e.preventDefault();
-			cosole.log(e.target);
-		})
-}
+for (var i = 0; i < product.length; i++) {
+	const a = document.createElement('a');
+	const img = document.createElement('img');
+	const linkText = document.createTextNode("my title text");
+	a.appendChild(linkText);
+
+	a.href = "#";
+	img.src = '#'; 
+		
+	product[i].appendChild(a);
+	a.appendChild(img);
+};
+
+
---
+++ products.js
@@ -0,0 +1,63 @@
+const abProducts = {
+    Hamburger: {
+      name: 'Hamburger',
+      img: 'menu-img/hamburger.jpg',
+      price: 130,
+    },
+    Cheeseburger: {
+      name: 'Cheeseburger',
+      img: 'menu-img/cheeseburger.jpg',
+      price: 150,
+    },
+    Pileci_batak: {
+      name: 'Pileći batak',
+      img: 'menu-img/pilecibatak.jpg',
+      price: 150,
+    },  
+    Pilece_belo: {
+      name: 'Pileće belo',
+      img: 'menu-img/pilecifile.jpg',
+      price: 180,
+    },  
+    Pohovana_piletina: {
+      name: 'Pohovana piletina',
+      img: 'menu-img/pohovanapiletina.jpg',
+      price: 130,
+    },  
+    Sendvic_riba_fileti: {
+      name: 'Sendvič riba fileti',
+      img: 'menu-img/ribafileti.jpg',
+      price: 150,
+    }, 
+    Lepinja_sunka_kackavalj: {
+      name: 'Lepinja šunka kačkavalj',
+      img: 'menu-img/lepinjasunkakackavalj.jpg',
+      price: 150,
+    }, 
+    Lepinja_kulen_kackavalj: {
+      name: 'Lepinja kulen kačkavalj',
+      img: 'menu-img/kulenkackavalj.jpg',
+      price: 150,
+    }, 
+    Lepinja_pecenica_kackavalj: {
+      name: 'Lepinja pečenica kačkavalj',
+      img: 'menu-img/pecenicakackavalj.jpg',
+      price: 150,
+    }, 
+    Lepinja_sunka_jaje: {
+      name: 'Lepinja šunka jaje',
+      img: 'menu-img/sunkajaje.jpg',
+      price: 150,
+    }, 
+    Pomfrit: {
+      name: 'Pomfrit',
+      img: 'menu-img/pomfrit.jpg',
+      price: 150,
+    }, 
+    Pizza: {
+      name: 'Pizza',
+      img: 'menu-img/pizza.jpg',
+      price: 150,
+    }, 
+  }
+  
