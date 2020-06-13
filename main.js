9feca8cf44ae35c2d316d727e2c44c91bc3f6d47
petarantic <57760707+petarantic@users.noreply.github.com>
2020-06-13 02:41:47 +0200

    Work is in progress, look and see if everything is fine

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-const product = document.getElementsByClassName('product');
 
-for (var i = 0; i < product.length; i++) {
+/*for (var i = 0; i < product.length; i++) {
 	const a = document.createElement('a');
 	const img = document.createElement('img');
 	const linkText = document.createTextNode("my title text");
@@ -14,3 +13,31 @@ for (var i = 0; i < product.length; i++) {
 };
 
 
+*/
+
+const allProducts = document.getElementsByClassName('allProducts');
+
+
+Object.keys(abProducts).forEach(function(item, index, array) {
+	chunkArray(item, index, array);
+	if (index === 0) {
+		array.length = array.indexOf(item);
+	};
+});
+
+function chunkArray(item, index, array) {
+	var perChunk = 4;
+	var result = array.reduce((resultArray, item, index) => { 
+		const chunkIndex = Math.floor(index/perChunk)
+	  
+		if(!resultArray[chunkIndex]) {
+		  resultArray[chunkIndex] = []
+		}
+	  
+		resultArray[chunkIndex].push(item)
+	  
+		return resultArray
+	  }, [])
+	  
+	  console.log(result);
+};
