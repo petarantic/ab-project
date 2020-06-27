b5f7c3274b95f20b960a630018df7a8d71d2ddc5
petarantic <57760707+petarantic@users.noreply.github.com>
2020-06-27 18:26:11 +0200

    Problem with duplicate product data in modal

--- main.js
+++ main.js
@@ -1,43 +1,64 @@
+const allProducts = document.getElementById('allProducts');
 
-/*for (var i = 0; i < product.length; i++) {
-	const a = document.createElement('a');
-	const img = document.createElement('img');
-	const linkText = document.createTextNode("my title text");
-	a.appendChild(linkText);
-
-	a.href = "#";
-	img.src = '#'; 
-		
-	product[i].appendChild(a);
-	a.appendChild(img);
-};
+const productsChunks = chunkArray(Object.keys(abProducts));
 
 
-*/
+productsChunks.forEach(function(productsInChunk, index) {
+	let rowDiv = document.createElement('div');
+	rowDiv.className = 'row';
+	allProducts.appendChild(rowDiv);
 
-const allProducts = document.getElementsByClassName('allProducts');
+	productsInChunk.forEach(function(product, index) {
+		let colDiv3 = document.createElement('div');
+		let a = document.createElement('a');
+		let img = document.createElement('img');
+		a.href = '#';
+		a.innerText = abProducts[product].name;
+		a.className = 'caption';
+		a.setAttribute("data-toggle", "modal");
+		a.setAttribute("data-target", "#myModal")
+		a.onclick = () => chooseProduct(product);
+		img.src = abProducts[product].img;
+		img.className = 'thumbnail';
+	  colDiv3.className = 'product col-md-3';
+  
+	  rowDiv.appendChild(colDiv3);
+		colDiv3.appendChild(img);
+		colDiv3.appendChild(a);
+	})
+  });
 
-
-Object.keys(abProducts).forEach(function(item, index, array) {
-	chunkArray(item, index, array);
-	if (index === 0) {
-		array.length = array.indexOf(item);
-	};
-});
-
-function chunkArray(item, index, array) {
-	var perChunk = 4;
-	var result = array.reduce((resultArray, item, index) => { 
+function chunkArray(array, perChunk = 4) {
+	return array.reduce((resultArray, item, index) => {
+		console.log('index glavnog niza', index) 
 		const chunkIndex = Math.floor(index/perChunk)
-	  
+
 		if(!resultArray[chunkIndex]) {
 		  resultArray[chunkIndex] = []
 		}
-	  
+		console.log('index u chunk-u', chunkIndex)
+
 		resultArray[chunkIndex].push(item)
-	  
-		return resultArray
-	  }, [])
-	  
-	  console.log(result);
+
+		console.log('chunks trenutno', resultArray)
+		return resultArray;
+	}, [])
 };
+
+function chooseProduct(product) { 
+	let captionDiv = document.getElementById('captionInfo');
+	let captionTitle = document.getElementById('captionTitle')
+		
+		let captionImg = document.createElement('img');
+		let captionPrice = document.createElement('p');
+  
+    	captionTitle.innerHTML = 'Ime proizvoda: ' + abProducts[product].name;
+    	captionImg.src = abProducts[product].img;
+    	captionPrice.innerHTML = 'Cena: ' + abProducts[product].price;
+
+    	captionImg.className = 'captionImg';
+    	captionPrice.className = 'captionPrice';
+
+    captionDiv.appendChild(captionImg);
+    captionDiv.appendChild(captionPrice);
+  };
